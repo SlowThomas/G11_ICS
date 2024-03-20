77979f5ed5791f32a23a492962bb83731161d8a8
SlowThomas <thomasxu070224@gmail.com>
2024-03-20 14:59:34 -0400

    Starting Unit 5 Assignment

---
+++ src/Unit_5/Bonus_Summarizer.java
@@ -0,0 +1,12 @@
+package Unit_5;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.ArrayList;
+
+public class Bonus_Summarizer {
+
+}
