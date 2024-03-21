a3637eeb5512c53f3331712a40a1cbd6021fc13b
SlowThomas <thomasxu070224@gmail.com>
2024-03-21 16:05:12 -0400

    Functionality of Unit 5 Assignment completed.

--- src/Unit_5/Bonus_Summarizer.java
+++ src/Unit_5/Bonus_Summarizer.java
@@ -1,5 +1,6 @@
 package Unit_5;
-
+// TODO: Write comments
+import java.io.FileWriter;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -13,8 +14,13 @@ public class Bonus_Summarizer {
         System.out.print("Year to retrieve data from: ");
         year = stdin.nextLine();
 
-        Scanner fin = new Scanner(new File(year + "_sales_summary.txt"));
-        PrintWriter fout = new PrintWriter(new File(year + "_ bonus_summary.txt"));
+        Scanner fin;
+        try { fin = new Scanner(new File(year + "_sales_summary.txt")); }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            return;
+        }
+        PrintWriter fout = new PrintWriter(new FileWriter(year + "_bonus_summary.txt"));
 
 
         double[] employee = new double[10000];
@@ -33,5 +39,23 @@ public class Bonus_Summarizer {
         }
         fin.close();
 
+        double bonus, sales_total = 0, bonus_total;
+        fout.printf("%-11s%15s%14s\n", "Employee ID", "Sales Total", "Bonus Total");
+        fout.println("****************************************");
+        for(int i = 0; i < num_employee; i++){
+            id = order[i];  amount = employee[id];  bonus = 0.05 * amount;
+            fout.printf("%-11d%15.2f%14.2f\n", id, amount, bonus);
+
+            sales_total += amount;
+        }
+        bonus_total = sales_total * 0.05;
+        fout.println("****************************************");
+        fout.printf("Number of Employees: %d\n", num_employee);
+        fout.printf("Sales Total: %.2f\n", sales_total);
+        fout.printf("Bonus Total: %.2f\n", bonus_total);
+
+        fout.close();
+
+        System.out.println("\nChanges have been applied");
     }
 }
