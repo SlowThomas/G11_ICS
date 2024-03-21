5407bec95c3da28510b4a7f3af220254a2229ba2
SlowThomas <thomasxu070224@gmail.com>
2024-03-21 15:12:13 -0400

    Working on Unit 5 Assignment

--- src/Unit_5/Bonus_Summarizer.java
+++ src/Unit_5/Bonus_Summarizer.java
@@ -5,8 +5,33 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.ArrayList;
-
 public class Bonus_Summarizer {
+    public static void main(String[] args) throws IOException {
+        Scanner stdin = new Scanner(System.in);
 
+        String year;
+        System.out.print("Year to retrieve data from: ");
+        year = stdin.nextLine();
+
+        Scanner fin = new Scanner(new File(year + "_sales_summary.txt"));
+        PrintWriter fout = new PrintWriter(new File(year + "_ bonus_summary.txt"));
+
+
+        double[] employee = new double[10000];
+        int[] order = new int[10000];
+        int num_employee = 0;
+
+        fin.nextLine();fin.nextLine();
+
+        int id;
+        double amount;
+        while(fin.hasNext()){
+            id = Integer.parseInt(fin.next());
+            amount = Double.parseDouble(fin.next());
+            if(employee[id] == 0) order[num_employee++] = id;
+            employee[id] += amount;
+        }
+        fin.close();
+
+    }
 }
