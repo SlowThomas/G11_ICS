b43a6cf63e989ab1400ae30060c0851c86109a80
SlowThomas <thomasxu070224@gmail.com>
2024-03-22 14:51:52 -0400

    Finished unit 5 assignment.

--- Unit_5/src/Bonus_Summarizer.java
+++ Unit_5/src/Bonus_Summarizer.java
@@ -1,4 +1,17 @@
-// TODO: Write Header Comments
+/*
+Name: Thomas Xu
+Date: March 22, 2024
+
+Description:
+
+Accumulate the sales amount to determine the bonus each employee is entitled for.
+Each employee receives 5% of bonus on the accumulated sales amount.
+
+The program streams through the xxxx_sales_summary.txt and generate a new text file
+xxxx_bonus_summary.txt, where xxxx represents the year the user chose to retrieve data from
+
+*/
+
 // import necessary modules
 import java.io.FileWriter;
 import java.util.Scanner;
@@ -7,50 +20,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Bonus_Summarizer {
-    public static void main(String[] args) throws IOException {
-        // Standard input stream
-        Scanner stdin = new Scanner(System.in);
 
+    // Arrays for storing the data
+    // employee[i] -> the total sales amount of the employee with id i.
+    // order[i] -> the id of the i-th employee that appeared on the original list.
+    static double[] employee = new double[10000];
+    static int[] order = new int[10000];
 
-        // Retrieve the target year
-        String year;
-        System.out.print("Year to retrieve data from: ");
-        year = stdin.nextLine();
-
-
-        // In/out stream for the input file and the output file
-        Scanner fin;
-        try { fin = new Scanner(new File(year + "_sales_summary.txt")); }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-            return;
-        }
-        PrintWriter fout = new PrintWriter(new FileWriter(year + "_bonus_summary.txt"));
-
-
-        // Arrays for storing the data
-
-        // employee[i] -> the total sales amount of the employee with id i.
-        // order[i] -> the id of the i-th employee that appeared on the original list.
-        double[] employee = new double[10000];
-        int[] order = new int[10000];
-
-
-        // Variables for storing key quantities
-
-        // num_employee -> total number of employees
-        // sales_total -> total sales amount by all employees
-        // bonus_total -> total bonus awarded to all employees
-
-        // id -> temporary variable, stores the id of the current entity
-        // amount -> temporary variable, stores the sales amount of the current entity
-        // bonus -> temporary variable, stores the bonus awarded for the current employee
-        int     id, num_employee = 0;
-        double  amount, bonus, sales_total = 0, bonus_total;
-
-
+    public static int record_data(Scanner fin){
         // dump the first two lines of the input file
         fin.nextLine();fin.nextLine();
+
+        // num_employee -> total number of employees
+        // id -> temporary variable, stores the id of the current entity
+        // amount -> temporary variable, stores the sales amount of the current entity
+        int id, num_employee = 0;
+        double amount;
 
 
         // Go through each entity of the input file's list
@@ -64,8 +49,21 @@ public class Bonus_Summarizer {
             // update the sales amount recorded for the employee
             employee[id] += amount;
         }
-        fin.close();
         // Finish going through the input file
+        fin.close();
+
+        // return the number of recorded employees
+        return num_employee;
+    }
+
+    public static void print_sheet(PrintWriter fout, int num_employee){
+        // id -> temporary variable, stores the id of the current entity
+        // amount -> temporary variable, stores the sales amount of the current entity
+        // bonus -> temporary variable, stores the bonus awarded for the current employee
+        // sales_total -> total sales amount by all employees
+        // bonus_total -> total bonus awarded to all employees
+        int id;
+        double  amount, bonus, sales_total = 0, bonus_total;
 
 
         // Print the header for the output file
@@ -91,6 +89,35 @@ public class Bonus_Summarizer {
 
         // save the changes to the output file
         fout.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Standard input stream
+        Scanner stdin = new Scanner(System.in);
+
+
+        // Retrieve the target year
+        String year;
+        System.out.print("Year to retrieve data from: ");
+        year = stdin.nextLine();
+
+
+        // In/out stream for the input file and the output file
+        Scanner fin;
+        try { fin = new Scanner(new File(year + "_sales_summary.txt")); }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+            return;
+        }
+        PrintWriter fout = new PrintWriter(new FileWriter(year + "_bonus_summary.txt"));
+
+
+        // num_employee -> total number of employees
+        int num_employee;
+        // Retrieve data from the sheet provided
+        num_employee = record_data(fin);
+        // Record the data to the output file
+        print_sheet(fout, num_employee);
 
 
         // notify the user for the program's completion
