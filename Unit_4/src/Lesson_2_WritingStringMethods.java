403a5cadf4976873d37d15e0025b5030ee5345cc
SlowThomas <thomasxu070224@gmail.com>
2024-03-26 14:26:04 -0400

    Lesson 2 of unit 4

--- Unit_4/src/Lesson_2_WritingStringMethods.java
+++ Unit_4/src/Lesson_2_WritingStringMethods.java
@@ -23,5 +23,56 @@ public class Lesson_2_WritingStringMethods {
     public static void main(String[] args){
         System.out.println(everyN("Markville", 3));
         System.out.println(everyN("Computer Science", 5));
+        System.out.println(addHyphens("Markville"));
+        System.out.println(remove("Computer", 'r'));
+        System.out.println(replace("School", 'o', '@'));
+    }
+
+    // Question 1
+    // Write a method addHyphens that takes a String str and returns
+    // a new String with hyphens (-) added in between each character
+    // of the String
+    // addHyphens("Markville") -> "M-a-r-k-v-i-l-l-e"
+    // addHyphens("Comp Sci")  -> "C-o-m-p- -S-c-i"
+
+    public static String addHyphens(String str){
+        String sb = "";
+        for(int i = 0; i < str.length() - 1; i++){
+            sb += str.charAt(i) + "-";
+        }
+        return sb + str.charAt(str.length() - 1);
+    }
+
+    // Question 2
+    // Write a method called remove which takes a String str and a character ch
+    // The method returns the string with all instances of ch removed
+    // remove("Computer", 'r') -> "Compute"
+    // remove("Banana", 'a') -> "Bnn"
+    public static String remove(String str, char ch){
+        String sb = "";
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != ch){
+                sb += str.charAt(i);
+            }
+        }
+        return sb;
+    }
+
+    // Question 3
+    // Write a method replace which takes a String str and two characters c1 and c2
+    // The method returns a copy of str where all instances of c1 replaced by c2
+    // replace("Markville", 'l', 'L') -> "MarkviLLe"
+    // replace("School", 'o', '@') -> "Sch@@l"
+    public static String replace(String str, char c1, char c2){
+        String sb = "";
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == c1){
+                sb += c2;
+            }
+            else{
+                sb += str.charAt(i);
+            }
+        }
+        return sb;
     }
 }
