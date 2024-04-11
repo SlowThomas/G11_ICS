1788c873d81c5735988af0264b7168ff7a3f3289
SlowThomas <thomasxu070224@gmail.com>
2024-04-10 21:00:24 -0400

    lesson 2 of unit 6

--- Final_Culminating_Task/src/engine/Scene.java
+++ Final_Culminating_Task/src/engine/Scene.java
@@ -4,18 +4,40 @@ import java.awt.image.*;
 import algebra.*;
 
 class Algorithm {
-    public static void rasterize(Matrix vertices, Vector colors, byte[][][] screen){
+    private static final double epsilon = 1E-10; // another choice: 1E-14
+
+    public static boolean point_in_triangle(int x, int y, double[][] triangle){
+        // TODO complete body
+        return false;
+    }
+    public static void rasterize(Matrix vertices, Vector color, byte[][][] screen){
         double[][] vect2 = {
                 {Screen.distance * vertices.at(0, 0) / vertices.at(0, 2), Screen.distance * vertices.at(1, 0) / vertices.at(1, 2), Screen.distance * vertices.at(2, 0) / vertices.at(2, 2)},
                 {Screen.distance * vertices.at(0, 1) / vertices.at(0, 2), Screen.distance * vertices.at(1, 1) / vertices.at(1, 2), Screen.distance * vertices.at(2, 1) / vertices.at(2, 2)},
                 {vertices.at(0, 2), vertices.at(1, 2), vertices.at(2, 2)}
         };
 
-        int l = Math.max((int)Math.min(Math.min(vect2[0][0], vect2[0][1]), vect2[0][2]), 0);
-        int r = Math.min((int)Math.max(Math.max(vect2[1][0], vect2[1][1]), vect2[1][2]), screen.length - 1);
-        int t = Math.max((int)Math.min(Math.min(vect2[1][0], vect2[1][1]), vect2[1][2]), 0);
-        int b = Math.min((int)Math.max(Math.max(vect2[1][0], vect2[1][1]), vect2[1][2]), screen[0].length - 1);
+        // Generate the equation of the plane: ax + by + cz = d
+        Vector normal = vertices.at(0).subtract(vertices.at(1)).cross(vertices.at(0).subtract(vertices.at(2)));
+        double a = normal.at(0);
+        double b = normal.at(1);
+        double c = normal.at(2);
+        double d = a * vertices.at(0, 0) + b * vertices.at(0, 1) + c * vertices.at(0, 2);
 
+        int left = Math.max((int)Math.min(Math.min(vect2[0][0], vect2[0][1]), vect2[0][2]), 0);
+        int right = Math.min((int)Math.max(Math.max(vect2[1][0], vect2[1][1]), vect2[1][2]), screen.length);
+        int top = Math.max((int)Math.min(Math.min(vect2[1][0], vect2[1][1]), vect2[1][2]), 0);
+        int bottom = Math.min((int)Math.max(Math.max(vect2[1][0], vect2[1][1]), vect2[1][2]), screen[0].length);
+
+        for(int i = left; i < right; i++){
+            for(int j = top; j < bottom; j++){
+                if(point_in_triangle(i, j, vect2) && Math.abs(c) > epsilon){
+                    screen[i][j] = new byte[]{
+                            (byte)color.at(0), (byte)color.at(1), (byte)color.at(2),
+                            }; // TODO: Add z-buffer
+                }
+            }
+        }
     }
 }
 
